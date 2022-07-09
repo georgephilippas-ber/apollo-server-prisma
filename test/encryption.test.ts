91f17abf44391585dd39d0d3b225c63f11352cca
georgephilippas-ber <george.philippas@outlook.com.gr>
2022-07-09 19:26:18 +0200

    Initial commit

---
+++ test/encryption.test.ts
@@ -0,0 +1,21 @@
+import {Encryption} from "../core/authentication/encryption/encryption";
+
+let encrypted_ = "random";
+
+let correct_guess_ = "random";
+let incorrect_guess_ = "rnd"
+
+describe("Encryption", () =>
+{
+    let hash_ = Encryption.hash(encrypted_);
+
+    it("verify (correct_guess_)", () =>
+    {
+        expect(Encryption.verify(correct_guess_, hash_)).toBeTruthy();
+    });
+
+    it("verify (incorrect_guess_)", () =>
+    {
+        expect(Encryption.verify(incorrect_guess_, hash_)).toBeFalsy();
+    })
+});
