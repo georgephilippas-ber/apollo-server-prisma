18cbab0785fabfec4902620dad6a652a930d03fd
georgephilippas-ber <george.philippas@outlook.com.gr>
2022-07-09 22:49:33 +0200

    Initial commit

--- test/utilities.test.ts
+++ test/utilities.test.ts
@@ -5,32 +5,35 @@ describe("utilities", () =>
     it("combine", () =>
     {
         let obj1 = {
-            a: 10,
-            b: undefined,
-            d: "string"
+            a: 1,
+            b: "string",
+            c: undefined,
+            d: null
         }
         let obj2 = {
-            d: 20,
-            f: 10
+            d: undefined,
+            e: null,
+            f: -1
         }
 
         expect(combine([obj1, obj2])).toHaveProperty(["a"]);
 
         expect(combine([obj1, obj2])).toHaveProperty(["b"]);
-        expect(combine([obj1, obj2]).b).toBeUndefined();
-
+        expect(combine([obj1, obj2])).toHaveProperty(["c"]);
+        expect(combine([obj1, obj2]).c).toBeUndefined();
         expect(combine([obj1, obj2])).toHaveProperty(["d"]);
+        expect(combine([obj1, obj2])).toHaveProperty(["e"]);
         expect(combine([obj1, obj2])).toHaveProperty(["f"]);
-
 
         expect(combine([obj1, obj2]).g).toBeUndefined();
     });
 
     it("toInteger", () =>
     {
-        expect(toInteger([20])).toBeUndefined();
-        expect(toInteger("20")).toEqual(20);
-        expect(toInteger("20b")).toEqual(20);
-        expect(toInteger("20b6")).toEqual(20);
+        expect(toInteger([0xff])).toBeUndefined();
+        expect(toInteger("100")).toEqual(100);
+        expect(toInteger("100b")).toEqual(100);
+        expect(toInteger("100b200")).toEqual(100);
+        expect(toInteger("b100")).toBeUndefined();
     })
 });
