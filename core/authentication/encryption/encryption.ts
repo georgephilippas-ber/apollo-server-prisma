e19ab6bcaa82de2003d3a919e81901b11f5c53a1
georgephilippas-ber <george.philippas@outlook.com.gr>
2022-07-09 19:18:06 +0200

    Initial commit

--- core/authentication/encryption/encryption.ts
+++ core/authentication/encryption/encryption.ts
@@ -1,23 +1,14 @@
 import * as bcrypt from 'bcrypt'
 
-import * as crypto from "crypto";
-
 export class Encryption
 {
-    public static hash_passkey(passkey_: string): string
+    public static hash(string_: string): string
     {
-        let hash_: crypto.Hash = crypto.createHash("sha512").update(passkey_);
-
-        return hash_.digest().toString("hex");
+        return bcrypt.hashSync(string_, 0x0f);
     }
 
-    public static hash_password(password_: string): string
+    public static verify(string_: string, string_hash_: string): boolean
     {
-        return bcrypt.hashSync(password_, 0x0f);
-    }
-
-    public static verify_password(password_: string, password_hash_: string): boolean
-    {
-        return bcrypt.compareSync(password_, password_hash_);
+        return bcrypt.compareSync(string_, string_hash_);
     }
 }
