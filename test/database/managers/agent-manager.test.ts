6411513865616b875555c3e97de477aa416aabbe
georgephilippas-ber <george.philippas@outlook.com.gr>
2022-07-10 05:29:56 +0200

    Initial commit

---
+++ test/database/managers/agent-manager.test.ts
@@ -0,0 +1,19 @@
+import {AgentManager, createManyAgentRandom} from "../../../src/database/managers/agent-manager";
+import {prismaClient} from "../../../src/database/database-provider";
+
+let agentManager = new AgentManager(prismaClient);
+
+beforeAll(async () =>
+{
+    await agentManager.delete_all();
+
+    await createManyAgentRandom(agentManager, 0x04);
+});
+
+describe("agent-manager", () =>
+{
+    it("count", async () =>
+    {
+        expect((await agentManager.all()).length).toBe(4);
+    })
+});
